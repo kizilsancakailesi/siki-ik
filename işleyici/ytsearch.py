e5d2f1b1d2d4ce304fed607544d8c983a58fad12
KIZILSANCAK AİLESİ <90479077+kizilsancakailesi@users.noreply.github.com>
2021-09-19 23:25:54 +0300

    Update and rename handler/ytsearch.py to işleyici/ytsearch.py

--- işleyici/ytsearch.py
+++ işleyici/ytsearch.py
@@ -20,7 +20,7 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 async def ytsearch(_, message: Message):
     try:
         if len(message.command) < 2:
-            await message.reply_text("/search needs an argument!")
+            await message.reply_text("/search needs an argumentt!")
             return
         query = message.text.split(None, 1)[1]
         m = await message.reply_text("Searching....")
